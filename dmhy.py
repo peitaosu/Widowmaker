901f137bd9b71fffe9dde2196b7c8959e9a0170c
Tony Su <peitaosu@163.com>
2018-01-22 21:58:49 +0800

    disable js in dmhy

--- dmhy.py
+++ dmhy.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 chrome_options = webdriver.ChromeOptions()
 prefs = {
     "profile.managed_default_content_settings.images": 2, 
-    "profile.managed_default_content_settings.javascript": 1
+    "profile.managed_default_content_settings.javascript": 2
     }
 chrome_options.add_experimental_option("prefs", prefs)
 driver = webdriver.Chrome(chrome_options=chrome_options)
