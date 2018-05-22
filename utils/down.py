e607971096a3b73b7156b9e43d2a0756376ea33a
Tony Su <peitaosu@163.com>
2018-05-22 13:16:38 +0800

    combine utils

--- utils/down.py
+++ utils/down.py
@@ -1,4 +1,6 @@
-import urllib2
+import os, sys, urllib2, thread
+from selenium import webdriver
+
 
 CHUNK_SIZE = 16 * 1024
 
@@ -21,3 +23,23 @@ def download(url, file_path, header):
         print "Download Finished."
     except Exception as err:
         print "Download Failed: {}".format(err)
+
+def download_image(image_link, local_file driver, request_headers):
+    try:
+        request = urllib2.Request(image_link, headers=request_headers)
+        img = urllib2.urlopen(request)
+        with open(local_file, 'wb') as save_file:
+            print local_file
+            save_file.write(img.read())
+    except urllib2.URLError,e:
+        print e.reason
+
+def get_pics_from_url(page_url, driver):
+    driver.get(page_url)
+    image_xpath = '//img'
+    image_elements = driver.find_elements_by_xpath(image_xpath)
+    for image_element in image_elements:
+        img_src = image_element.get_attribute('src')
+        thread.start_new_thread(download_image, (img_src, img_src.split("/")[-1], ) )
+
+
