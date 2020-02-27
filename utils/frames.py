50890bf773ae203c8c0289b967166a3e509fe909
Tony Su <peitaosu@163.com>
2020-02-27 09:59:24 +0800

    fix ffmpeg cannot find file if file path has space

--- utils/frames.py
+++ utils/frames.py
@@ -6,7 +6,7 @@ def create_frames(video_path, percent, output_path):
     out_folder = os.path.join(output_path, video_name)
     if not os.path.isdir(out_folder):
         os.makedirs(out_folder)
-    os.system("ffmpeg -i {} -f image2 -vf fps=fps={} {}_%d.jpg".format(video_path, percent, out_folder + "\\" + video_name))
+    os.system("ffmpeg -i \"{}\" -f image2 -vf fps=fps={} {}_%d.jpg".format(video_path, percent, out_folder + "\\" + video_name))
 
 if __name__=="__main__":
     video_path = sys.argv[1]
