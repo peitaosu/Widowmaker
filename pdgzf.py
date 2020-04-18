97b6498aa1c377c8b585ca4fc70d5a10765bfef7
Tony Su <peitaosu@163.com>
2020-04-18 22:19:10 +0800

    add script for pdgzf

---
+++ pdgzf.py
@@ -0,0 +1,11 @@
+import os, sys, time
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+prefs = {
+    "profile.managed_default_content_settings.images": 2, 
+    "profile.managed_default_content_settings.javascript": 1,
+}
+
+def get_house_status():
+    pass
