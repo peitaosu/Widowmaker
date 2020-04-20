0340bc601e4ab73ddf0fbf75ddaea65324ef9301
Tony Su <peitaosu@163.com>
2020-04-20 22:11:52 +0800

    fix issues

--- pdgzf.py
+++ pdgzf.py
@@ -1,20 +1,18 @@
+# -*- coding: utf-8 -*-
 import os, sys, time
 from selenium import webdriver
 
-chrome_options = webdriver.ChromeOptions()
-prefs = {
-    "profile.managed_default_content_settings.images": 2, 
-    "profile.managed_default_content_settings.javascript": 1,
-}
-chrome_options.add_experimental_option("prefs", prefs)
-driver = webdriver.Chrome(chrome_options=chrome_options)
+driver = webdriver.Chrome()
 
-def get_house_status(house_type):
+def get_house_status():
     page_url = "https://select.pdgzf.com/houseLists"
     driver.get(page_url)
-    house_xpath = '//li[@text="{}"]'.format(house_type)
-    house_element = driver.find_element_by_xpath(house_xpath)
-    house_element.click()
+    filter_xpath = '//ul[@class="clearfix fl"]'
+    filter_element = driver.find_elements_by_xpath(filter_xpath)[2]
+    type_element = filter_element.find_elements_by_tag_name("li")[2]
+    type_element.click()
+    time.sleep(5)
     house_count_xpath = '//h4[@class="fs26 c-6 village-house-tit"]/span'
     house_count_element = driver.find_element_by_xpath(house_count_xpath)
     return house_count_element.text
+
