37a18cfca3e93dff98b8d96d3eb386b445abb4d2
Tony Su <peitaosu@163.com>
2020-04-21 21:02:00 +0800

    support parameter

--- pdgzf.py
+++ pdgzf.py
@@ -4,15 +4,17 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 
-def get_house_status():
+def get_house_status(filter=2, type=2):
     page_url = "https://select.pdgzf.com/houseLists"
     driver.get(page_url)
     filter_xpath = '//ul[@class="clearfix fl"]'
-    filter_element = driver.find_elements_by_xpath(filter_xpath)[2]
-    type_element = filter_element.find_elements_by_tag_name("li")[2]
+    filter_element = driver.find_elements_by_xpath(filter_xpath)[filter]
+    type_element = filter_element.find_elements_by_tag_name("li")[type]
     type_element.click()
     time.sleep(5)
     house_count_xpath = '//h4[@class="fs26 c-6 village-house-tit"]/span'
     house_count_element = driver.find_element_by_xpath(house_count_xpath)
     return house_count_element.text
 
+if int(get_house_status(2, 2)) > 0:
+    print("New Source Found!") 
