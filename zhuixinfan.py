f8a2a230df17106d942b45c1f8d7fc899835d8d2
Tony Su <peitaosu@163.com>
2020-04-25 22:36:07 +0800

    get list from zhuixinfan

---
+++ zhuixinfan.py
@@ -0,0 +1,9 @@
+import os, sys, time
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+def get_sub_pages(list_page):
+    driver.get(list_page)
+    list_xpath = '//td[@class="td2"]/a'
+    list_elements = driver.find_elements_by_xpath(list_xpath)
