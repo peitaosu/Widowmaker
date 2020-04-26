0b5b0133ecd3b8e3a4b6e4dbabebbb14bcbda9bb
Tony Su <peitaosu@163.com>
2020-04-27 00:02:44 +0800

    get magnet links

--- zhuixinfan.py
+++ zhuixinfan.py
@@ -8,3 +8,9 @@ def get_sub_pages(list_page):
     list_xpath = '//td[@class="td2"]/a'
     list_elements = driver.find_elements_by_xpath(list_xpath)
     return [list.get_attribute("href") for list in list_elements]
+
+def get_magnet(page_url):
+    driver.get(page_url)
+    magnet_xpath = '//dd[@id="torrent_url"]'
+    magnet_element = driver.find_element_by_xpath(magnet_xpath)
+    print(magnet_element.text)
