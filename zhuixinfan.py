1bbd19c341c9261aa730a713a5eb5eeb1b4d9cde
Tony Su <peitaosu@163.com>
2020-04-26 23:00:11 +0800

    return list of sub pages

--- zhuixinfan.py
+++ zhuixinfan.py
@@ -7,3 +7,4 @@ def get_sub_pages(list_page):
     driver.get(list_page)
     list_xpath = '//td[@class="td2"]/a'
     list_elements = driver.find_elements_by_xpath(list_xpath)
+    return [list.get_attribute("href") for list in list_elements]
