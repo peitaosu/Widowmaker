5eb6530c4748d24e05ac711ecadfb82b15e10429
Tony Su <peitaosu@163.com>
2020-05-08 22:17:45 +0800

    fix issue

--- helper.py
+++ helper.py
@@ -2,7 +2,8 @@ class Helper():
     def __init__(self):
         self.driver = webdriver.Chrome()
 
-    def get_element_by_xpath(page, xpath):
+    def get_element_by_xpath(self, page, xpath):
         driver.get(page)
         element = driver.find_element_by_xpath(xpath)
         return element
+    
