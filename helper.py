824fb0529255df5c01d8743a960804404e3ee114
Tony Su <peitaosu@163.com>
2020-05-09 23:14:11 +0800

    use helper in zhuixinfan.py

--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
+from selenium import webdriver
+
 class Helper():
     def __init__(self):
         self.driver = webdriver.Chrome()
@@ -6,4 +8,9 @@ class Helper():
         driver.get(page)
         element = driver.find_element_by_xpath(xpath)
         return element
+    
+    def get_elements_by_xpath(self, page, xpath):
+        driver.get(page)
+        elements = driver.find_elements_by_xpath(xpath)
+        return elements
     
--- zhuixinfan.py
+++ zhuixinfan.py
@@ -1,18 +1,16 @@
 import os, sys, time
-from selenium import webdriver
+from helper import Helper
 
-driver = webdriver.Chrome()
+helper = Helper()
 
 def get_sub_pages(list_page):
-    driver.get(list_page)
     list_xpath = '//td[@class="td2"]/a'
-    list_elements = driver.find_elements_by_xpath(list_xpath)
+    list_elements = helper.get_elements_by_xpath(list_page, list_xpath)
     return [list.get_attribute("href") for list in list_elements]
 
 def get_magnet(page_url):
-    driver.get(page_url)
     magnet_xpath = '//dd[@id="torrent_url"]'
-    magnet_element = driver.find_element_by_xpath(magnet_xpath)
+    magnet_element = helper.get_elements_by_xpath(page_url, magnet_xpath)
     return magnet_element.text
 
 def save_magnet_to_file(out_file, magnet):
